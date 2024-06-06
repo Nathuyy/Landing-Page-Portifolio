38bbfc7076a10933f12f64402b9ce03559993dc1
nathy <nathalia.cericatto@gmail.com>
2024-06-06 19:31:24 -0300

    uptade

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ function redirecionar(url){
     window.location.href = url
 }
 
+const botoesRepositorio = document.querySelectorAll('.repositorio');
+
+botoesRepositorio.forEach(button => {
+    button.addEventListener('click', function() {
+        const projectName = this.parentElement.parentElement.querySelector('h3').innerText;
+        let urlRepositorio;
+        switch (projectName) {
+            case 'Automatização de Planilhas':
+                urlRepositorio = 'https://github.com/Nathuyy/Automatizacao-de-Planilhas';
+                break;
+            case 'Controle de Estoque em Java':
+                urlRepositorio = 'https://github.com/Nathuyy/controle-de-estoque';
+                break;
+            case 'Color Flipper':
+                urlRepositorio = 'https://github.com/Nathuyy/FullStack-Projects/tree/main/color-flipper';
+                break;
+        }
+
+        window.location.href = urlRepositorio;
+        
+    });
+
+});
